599e2b93dd2af6b18e45a1353a00cc9fe45ec4ab
unknown <sld5434@IST113-18.win.psu.edu>
2015-09-20 17:12:40 -0400

    comments added

--- p1/matlab/ImNorm.m
+++ p1/matlab/ImNorm.m
@@ -1,9 +1,13 @@
+% returns matrix out - the result of the image normalization
+% inputs: in - image to be normalized
+%         N and M - dimensions of input image
 function out = ImNorm(in, N, M)
 in = double(in);
 out = double(in);
 for i=1:N
     for j=1:M
         for k=1:3
+            % normalize
             out(i,j,k) = in(i,j,k)/255.0 - 0.5;
         end
     end
