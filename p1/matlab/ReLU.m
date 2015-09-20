dc9c3670a65973aa196cf06d4c79fadb11fbfef1
sdurofchalk <sld5434@IST113-18.win.psu.edu>
2015-09-20 17:24:04 -0400

    comments added

--- p1/matlab/ReLU.m
+++ p1/matlab/ReLU.m
@@ -1,8 +1,12 @@
+% returns matrix out - the result of the rectified linear unit
+% inputs: in - input image
+%         N,M,D - dimensions of the input image
 function out = ReLU(in, N, M, D)
 out = double(in);
 for i=1:N
     for j=1:M
         for k=1:D
+            % any negative numbers become 0 in the output
             out(i,j,k) = max(in(i,j,k), 0);
         end
     end
--- p1/matlab/Softmax.m
+++ p1/matlab/Softmax.m
@@ -1,11 +1,16 @@
+% returns matric out - the result of the softmax
+% inputs: in - input image
+%         D - third dimension of the input image
 function out = Softmax(in, D)
 out = double(in);
+% calculate maximum across all values in the input
 a = max(in(1,1,:));
 sum = 0;
 for k = 1:D
     sum = sum+ exp(in(1,1,k)-a);
 end
 for k = 1:D
+    % normalize all values to be between 0 and 1 and sum to 1
     out(1,1,k) = exp(in(1,1,k)-a)/sum;
 end
 
