9294577538d907c36c1717da3ae37bc2b7583508
RWails <ryanswails@gmail.com>
2015-09-20 20:59:20 -0400

    finished demo code

--- p1/matlab/NeuralNetDemo.m
+++ p1/matlab/NeuralNetDemo.m
@@ -12,7 +12,7 @@ probOutput = zeros(numImages, 10);
 % j - cnn output
 classes = zeros(10, 10);
 
-for i=1:numImages
+for i=1:1
     img = imageset(:,:,:,i);
     %img = imrgb; % CHANGE ME\
     
@@ -23,89 +23,82 @@ for i=1:numImages
     
     % Layer 2 - Convolve
     img = convolve(img, filterbanks{2}, biasvectors{2});
+    ShowImline(img, 'Layer 2');
     
     % Layer 3 - ReLU
     imgDim = size(img);
     
-    figure('name', 'Layer 2');
-    imgline = img(:,:,1);
-    for p=2:imgDim(3)
-        imgline = horzcat(imgline, img(:,:,p));
-    end
-    imagesc(imgline); colormap('gray');
-    truesize;
-    
     img = ReLU(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 3'); imagesc(img);
+    ShowImline(img, 'Layer 3');
     
     % Layer 4 - Convolve
     img = convolve(img, filterbanks{4}, biasvectors{4});
-    figure('name', 'Layer 4'); imagesc(img);
+    ShowImline(img, 'Layer 4');
     
     % Layer 5 - ReLU
     imgDim = size(img);
     img = ReLU(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 5'); imagesc(img);
+    ShowImline(img, 'Layer 5');
     
     % Layer 6 - Max Pool
     imgDim = size(img);
     img = Maxpool(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 6'); imagesc(img);
+    ShowImline(img, 'Layer 6');
     
     % Layer 7 - Convolve
     img = convolve(img, filterbanks{7}, biasvectors{7});
-    figure('name', 'Layer 7'); imagesc(img);
+    ShowImline(img, 'Layer 7');
     
     % Layer 8 - ReLU
     imgDim = size(img);
     img = ReLU(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 8'); imagesc(img);
+    ShowImline(img, 'Layer 8');
     
     % Layer 9 - Convolve
     img = convolve(img, filterbanks{9}, biasvectors{9});
-    figure('name', 'Layer 9'); imagesc(img);
+    ShowImline(img, 'Layer 9');
     
     % Layer 10 - ReLU
     imgDim = size(img);
     img = ReLU(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 10'); imagesc(img);
+    ShowImline(img, 'Layer 10');
     
     % Layer 11 - Max Pool
     imgDim = size(img);
     img = Maxpool(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 11'); imagesc(img);
+    ShowImline(img, 'Layer 11');
     
     % Layer 12 - Convolve
     img = convolve(img, filterbanks{12}, biasvectors{12});
-    figure('name', 'Layer 12'); imagesc(img);
+    ShowImline(img, 'Layer 12');
     
     % Layer 13 - ReLU
     imgDim = size(img);
     img = ReLU(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 13'); imagesc(img);
+    ShowImline(img, 'Layer 13');
     
     % Layer 14 - Convolve
     img = convolve(img, filterbanks{14}, biasvectors{14});
-    figure('name', 'Layer 14'); imagesc(img);
+    ShowImline(img, 'Layer 14');
     
     % Layer 15 - ReLU
     imgDim = size(img);
     img = ReLU(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 15'); imagesc(img);
+    ShowImline(img, 'Layer 15');
     
     % Layer 16 - Max Pool
     imgDim = size(img);
     img = Maxpool(img, imgDim(1), imgDim(2), imgDim(3));
-    figure('name', 'Layer 16'); imagesc(img);
+    ShowImline(img, 'Layer 16');
     
     % Layer 17 - Fully Connected
     img = FuCd(img, filterbanks{17}, biasvectors{17});
-    figure('name', 'Layer 17'); imagesc(img);
+    figure('name', 'Layer 17'); bar(squeeze(img(:,:,1:end)));
     
     % Layer 18 - Soft Max
     imgDim = size(img);
     img = Softmax(img, imgDim(3));
-    figure('name', 'Layer 18'); imagesc(img);
+    figure('name', 'Layer 18'); bar(squeeze(img(:,:,1:end)));
     
     % Map the output
     probOutput(i,:) = img;
--- p1/matlab/demo.m
+++ p1/matlab/demo.m
@@ -4,5 +4,6 @@ load('debuggingTest.mat');
 
 imageset = zeros(32, 32, 3, 2);
 imageset(:,:,:,1) = imrgb;
+figure('name', 'Original'); imagesc(imrgb);
 
 out = NeuralNetDemo(imageset, filterbanks, biasvectors, trueclass);
