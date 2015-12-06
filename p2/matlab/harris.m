a5d6bb528700bcb179e4a5d1b38bf8e3fad679d4
RWails <ryanswails@gmail.com>
2015-12-05 20:35:38 -0500

    harris incomplete

--- p2/matlab/harris.m
+++ p2/matlab/harris.m
@@ -2,6 +2,8 @@ function [ output_args ] = harris( img, xmin, xmax, ymin, ymax )
 %HARRIS Summary of this function goes here
 %   Detailed explanation goes here
 
+k = 0.05;
+
 img = rgb2gray(img);
 img = double(img);
 
@@ -17,8 +19,28 @@ i_x2 = i_x .* i_x;
 i_y2 = i_y .* i_y;
 i_xy = i_x .* i_y;
 
+gauss = gausswin(9);
+gauss = imfilter(gauss, gauss', 'full');
+
+s_x2 = imfilter(i_x2, gauss);
+s_y2 = imfilter(i_y2, gauss);
+s_xy = imfilter(i_xy, gauss);
+
+r = imgPatch;
+
+for i = 1:(ymax - ymin + 1)
+    for j = 1:(xmax - xmin + 1)
+        h = [ s_x2(i, j) s_xy(i, j); s_xy(i, j) s_y2(i, j) ];
+        t = trace(h);
+        d = det(h);
+        
+        r(i, j) = d - k * t * t;
+    end
+end
+
 colormap(gray);
-imagesc(i_xy);
+imagesc(r);
+% imagesc(i_xy);
 
 end
 
