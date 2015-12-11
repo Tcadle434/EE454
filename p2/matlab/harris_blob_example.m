cdff29f5a68397654059452f5330fb6ea6bb29d7
RWails <ryanswails@gmail.com>
2015-12-10 19:27:41 -0500

    fixed harris blob example

--- p2/matlab/harris_blob_example.m
+++ p2/matlab/harris_blob_example.m
@@ -2,9 +2,9 @@
 sample = imread('../frames/DaMultiview-seq7022.png');
 harris_img = harris(sample, 90, 140, 100, 300);
 colormap(gray);
-corners = blob2px(harris_img, 50);
+imagesc(harris_img);
+corners = blob2px(harris_img, 45);
 
-image(sample);
 hold on;
 for k = 1 : numel(corners) / 2
     plot(corners(1,k), corners(2,k), 'r*');
