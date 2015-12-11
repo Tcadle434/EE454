53d7eb6d39cee5962b95dc531f4baa2c60e32f8e
psucurran <spc5213@gmail.com>
2015-12-11 16:06:23 -0500

    fixes

--- p2/matlab/SumOfSqaureDifferences.m
+++ p2/matlab/SumOfSqaureDifferences.m
@@ -1,5 +1,6 @@
 function [ ssd ] = SumOfSqaureDifferences( i1,i2,p1,p2,x,y,w1,w2 )
-    ssd = 0;
+    %make this a long?    
+    ssd = double(0);
     t = 0;
     p1(1) = p1(1)-y/2;
     p1(2) = p1(2)-x/2;
@@ -8,15 +9,37 @@ function [ ssd ] = SumOfSqaureDifferences( i1,i2,p1,p2,x,y,w1,w2 )
     
     dim1 = size(i1);
     dim2 = size(i2);
+    
+    w1xmin = w1(3);
+    w1ymin = w1(4);
+    w2xmin = w2(3);
+    w2ymin = w2(4);
+    
+    
+    p1x = p1(2);
+    p1y = p1(1);
+    p2x = p2(2);
+    p2y = p2(1);
+    
+    ip1x = w1xmin+p1x;
+    ip1y = w1ymin+p1y;
+    ip2x = w2xmin+p2x;
+    ip2y = w2ymin+p2y;
+    
     for i=1:x
       for j=1:y
           for k=1:3
                 %check this because there was an index out of bounds error
-                if ~(w1(4)+p1(1)+ j > dim1(1) || w1(3)+p1(2)+i > dim1(2) || ...
-                       w2(4)+p2(1)+j > dim2(1) || w2(3)+p2(2)+i > dim2(1))
-                    
-                   ssd = ssd + (i1(w1(4)+p1(1)+j,w1(3)+p1(2)+i,k)...
-                    -i2(w2(4)+p2(1)+j,w2(3)+p2(2)+i,k))^2;     
+                if ~(ip1y + j> dim1(1) || ip1x + i > dim1(2) || ...
+                       ip2y + j > dim2(1) || ip2x + i > dim2(2))
+                   
+                   pixel1 = i1(ip1y+j,ip1x+i,k);
+                   pixel2 = i2(ip2y+j,ip2x+i,k);
+                   
+                   step1 = double(pixel1)-double(pixel2);
+                   step2 = step1^2;
+                   
+                   ssd = ssd + step2;
                 end
                 
           end
