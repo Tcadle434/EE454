fadd667e99ddb81ca3fe0efd7a21387d852b33d2
psucurran <spc5213@gmail.com>
2015-12-10 22:53:27 -0500

    barf matcher is "done"

--- p2/matlab/main.m
+++ p2/matlab/main.m
@@ -23,6 +23,7 @@ for n = 1:numOfFrames
         filenameArr{end+1} = filename;
         disp(filenameArr(n));
         image = imread(filename);
+        
     
         %search for desired frames
         r = 1;
@@ -90,32 +91,6 @@ end
 
 
 
-%boxes %corners
-%{
-i1[][]
-i2[][]
-i1boxes
-i2boxes
-cornerpatchx
-cornerpatchy
-%match up corners
-    boxmatchmatrix = zeros(i1boxes,i2boxes);
-    for b1 = 1:i1boxes
-        for b2 = 1:i2boxes
-            i1corners
-            i2corners
-            cornermatchmatrix = zeros(i1corner,i2corners);
-            for c1 = 1:i1corners
-               for c2 = 1:i2corners
-                    ssd = SumOfSquareDifferences(i1(b1,c1),i2(b2,c2),...
-                        cornerpatchx,cornerpatchy);
-                    cornermatchmatrix(c1,c2) = ssd;
-               end
-            end
-            [acorners,ccorners] = assignmentoptimal(cornermatchmatrix);
-            boxmatchmatrix(b1,b2) = ccorners;
-        end
-    end
-    [aboxes,cboxes] = assignmentoptimal(boxmatchmatrix);
+
     
- %}   
+    
