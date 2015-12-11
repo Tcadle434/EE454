4b32f46bebef8f4786b58c104f7af5045d6f6c94
psucurran <spc5213@gmail.com>
2015-12-11 01:34:19 -0500

    Found a bug
    the input to the hungarian algortithm for box matching contained far too
    many zeros

--- p2/matlab/main.m
+++ p2/matlab/main.m
@@ -1,14 +1,18 @@
 %fill gtboxarray which has window information ie. max's and min's
 parseGroundTruthSupressed;
 
-numOfFrames = 3; %set number frames 
+%temp = gtboxarray(1,:);
+%gtboxarray(1,:)=gtboxarray(2,:);
+%gtboxarray(2,:)=temp;
+
+numOfFrames = 2; %set number frames 
 frameSkipRate = 1; %distance between next frame chosen, greater the distance more of a change of windows
 frameindex = 7022;  %starting frame; range is 7024-7200s
 filepath = '../frames/DaMultiview-seq';
-threshold = 140;
-alpha = 0.15;
-cornerPatchSizeX = 8;
-cornerPatchSizeY = 8;
+threshold = 220;
+alpha = 0.5;
+cornerPatchSizeX = 6;
+cornerPatchSizeY = 6;
 
 [corners, sFrameArr] = phase1(numOfFrames, frameSkipRate, frameindex,...
     gtboxarray, filepath, threshold, alpha);
