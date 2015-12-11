c23a2f48cd23d4313683775ad99d3468f02b93a6
psucurran <spc5213@gmail.com>
2015-12-11 00:56:55 -0500

    Working well?

--- p2/matlab/main.m
+++ p2/matlab/main.m
@@ -1,11 +1,11 @@
 %fill gtboxarray which has window information ie. max's and min's
 parseGroundTruthSupressed;
 
-numOfFrames = 2; %set number frames 
+numOfFrames = 3; %set number frames 
 frameSkipRate = 1; %distance between next frame chosen, greater the distance more of a change of windows
 frameindex = 7022;  %starting frame; range is 7024-7200s
 filepath = '../frames/DaMultiview-seq';
-threshold = 100;
+threshold = 140;
 alpha = 0.15;
 cornerPatchSizeX = 8;
 cornerPatchSizeY = 8;
@@ -13,6 +13,7 @@ cornerPatchSizeY = 8;
 [corners, sFrameArr] = phase1(numOfFrames, frameSkipRate, frameindex,...
     gtboxarray, filepath, threshold, alpha);
 
+%{
 harris_img = imread('../frames/DaMultiview-seq7022.png');
 imshow(harris_img);
 
@@ -22,6 +23,7 @@ for k = 1 : numel(cs) / 2
     plot(cs(1,k), cs(2,k), 'r*');
 end
 hold off;
+%}
 
 [cornerMatches, windowMatches] = matcher (corners, sFrameArr, numOfFrames,...
     gtboxarray, filepath, frameindex, cornerPatchSizeX, cornerPatchSizeY);
