0110b0b60ae05cec78f3e8bbe19a32640fd8b7cf
psucurran <spc5213@gmail.com>
2015-12-11 21:16:11 -0500

    main updates

--- p2/matlab/main.m
+++ p2/matlab/main.m
@@ -1,57 +1,35 @@
+%Parameters
+numOfFrames = 10; %set number frames 
+frameSkipRate = 1; %frame comparison distance
+frameindex = 7170;  %starting frame; range is 7022-7200
+filepath = '../frames/DaMultiview-seq';
+threshold = 235; %R threshold for harris corners
+alpha = 255; %controls how you want to choose corners at random
+                %small alpha makes corners with larger R's more 
+                %likely to be selected
+                %255 turns the "probability weighting" feature off
+%size of the patches to use around corners when matching
+cornerPatchSizeX = 12;
+cornerPatchSizeY = 12;
+
 %fill gtboxarray which has window information ie. max's and min's
 parseGroundTruthSupressed;
 
-%temp = gtboxarray(1,:);
-%gtboxarray(1,:)=gtboxarray(2,:);
-%gtboxarray(2,:)=temp;
-
-numOfFrames = 10; %set number frames 
-frameSkipRate = 1; %distance between next frame chosen, greater the distance more of a change of windows
-frameindex = 7170;  %starting frame; range is 7024-7200s
-filepath = '../frames/DaMultiview-seq';
-threshold = 235;
-alpha = 0.15;
-cornerPatchSizeX = 12;
-cornerPatchSizeY = 12;
 %get boxes and features (harris corners)
 [corners, sFrameArr] = phase1(numOfFrames, frameSkipRate, frameindex,...
     gtboxarray, filepath, threshold, alpha);
 
-%{
-harris_img = imread('../frames/DaMultiview-seq7022.png');
-imshow(harris_img);
-
-hold on
-cs = corners{7};
-for k = 1 : numel(cs) / 2
-    plot(cs(1,k), cs(2,k), 'r*');
-end
-hold off;
-%}
-%match
+%match corners and match boxes
 [cornerMatches, windowMatches] = matcher (corners, sFrameArr, numOfFrames,...
     gtboxarray, filepath, frameindex, cornerPatchSizeX, cornerPatchSizeY);
 
-
 %Compute Accuracy
-count = 0;
-count2 = 0;
-for i=1:9
-    dims = size(windowMatches{i});
-    for j=1:dims(1)
-       if windowMatches{i}(j) == j
-           count = count + 1;
-       end
-       count2 = count2+1;
-    end
-end
+accuracy = Acurrator(windowMatches,gtboxarray,sFrameArr);
+disp( sprintf( ...
+    'Windows were correctly matched between sequential images %2.2f%% of the time.',...
+    (accuracy)*100 ) );
 
-display(count);
-display(count2);
-display(double(count)/double(count2));
-
-display(Acurrator(windowMatches,gtboxarray,sFrameArr));
-%Ransac
+%Ransac to get translations between matched windows
 translations={};
 translationsByWindow = {};
 groundTruthTranslations;
@@ -65,4 +43,3 @@ for i=1:size(cornerMatches,2)
     end
 end
     
-
